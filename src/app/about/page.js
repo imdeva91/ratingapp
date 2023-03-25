af7a94dc542dedfbba7226cbd2e68f5721343dd8
imnikku <imnikku11@gmail.com>
2023-03-25 22:20:27 +0530

    first commit

---
+++ src/app/about/page.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <div className="mx-w-6xl mx-auto space-y-4 p-4 ">
+        <h1 className="text-2xl font-medium text-amber-700">About Us</h1>
+        <p>
+          it is a long established fact that a reader will be distracted by the
+          readable content of a page when looking at its layout. The point of
+          using Lorem Ipsum is that it has a more-or-less normal distribution of
+          letters,{" "}
+        </p>
+        <p>
+          it is a long established fact that a reader will be distracted by the
+          readable content of a page when looking at its layout. The point of
+          using Lorem Ipsum is that it has a more-or-less normal distribution of
+          letters,{" "}
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
